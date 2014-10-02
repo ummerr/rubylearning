a12cfddb45ebfe43d4aaf5c0776e344692ad9a33
Amar Singh <ummerr@gmail.com>
2014-10-01 17:13:46 -0700

    learning yo

--- summarize.rb
+++ summarize.rb
@@ -10,3 +10,4 @@ one_third = sentences_sorted.length / 3
 ideal_sentences = sentences_sorted.slice(one_third, one_third + 1)
 ideal_sentences = ideal_sentences.select { |sentence| sentence =~ /is|are/ }
 puts ideal_sentences.join(". ")
+
